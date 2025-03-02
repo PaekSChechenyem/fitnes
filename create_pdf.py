a1d67f8be96238354e828d70f9028d793bc4788f
PaekSChechenyem <108612572+PaekSChechenyem@users.noreply.github.com>
2025-03-02 09:08:55 +0300

    Update create_pdf.py

--- create_pdf.py
+++ create_pdf.py
@@ -1,8 +1,9 @@
 from markdown_pdf import MarkdownPdf, Section
 
-pdf = MarkdownPdf(toc_level=2)
 
 #сохранение текста с markdown-разметкой в файле pdf
 async def save_pdf(text, user_id):
+    pdf = MarkdownPdf(toc_level=2)
+
     pdf.add_section(Section(text, toc=False))
     pdf.save(f"{user_id}.pdf")
